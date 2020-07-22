5f57bf4136dc76e3bdb8f08455a2f32ddefbce65
Antoine Gannat <antoine.gannat@epitech.eu>
2020-07-22 20:55:51 +0200

    improve styling

--- src/components/Gallery/Gallery.web.tsx
+++ src/components/Gallery/Gallery.web.tsx
@@ -27,110 +27,109 @@ function Gallery(props: DefaultPropsWithTranslation): JSX.Element {
 
   return (
     <section id="gallery">
-      <div className="col-12">
-        <CategoryTitle title={translate("gallery")} />
-        <div className="gallery row col-12">
-          <button
-            className="prev"
-            hidden={page - 1 < 0}
-            onClick={() => changePage(page - 1)}
-          >
-            <i className="fas fa-chevron-left fa-2x"></i>
-          </button>
-          <div
-            key="gallery-page-1"
-            className={cssMerge(
-              "page-container row",
-              page !== 0
-                ? page >= lastPage
-                  ? "hide-slide-left"
-                  : "hide-slide-right"
-                : "visible"
-            )}
-          >
-            <div className="vertical-img left col-5">
+      <CategoryTitle title={translate("gallery")} />
+      <div className="gallery row col-12">
+        <button
+          className="prev"
+          hidden={page - 1 < 0}
+          onClick={() => changePage(page - 1)}
+        >
+          <i className="fas fa-chevron-left fa-2x"></i>
+        </button>
+        <div
+          key="gallery-page-1"
+          className={cssMerge(
+            "page-container row",
+            page !== 0
+              ? page >= lastPage
+                ? "hide-slide-left"
+                : "hide-slide-right"
+              : "visible"
+          )}
+        >
+          <div className="vertical-img left col-5">
+            <img
+              src="/images/pool/picture_1.jpg"
+              role="button"
+              alt={translate("poolAlt")}
+              onClick={() => setSlide("pool")}
+            />
+            {expandButton}
+          </div>
+          <div className="row col-7 horizontal-container">
+            <div className="horizontal-img">
               <img
-                src="/images/pool/picture_1.jpg"
+                src="/images/bedrooms/picture_6.jpg"
                 role="button"
-                alt={translate("poolAlt")}
-                onClick={() => setSlide("pool")}
+                alt={translate("bedroomAlt")}
+                onClick={() => setSlide("bedrooms")}
               />
               {expandButton}
             </div>
-            <div className="row col-7 horizontal-container">
-              <div className="horizontal-img">
-                <img
-                  src="/images/bedrooms/picture_6.jpg"
-                  role="button"
-                  alt={translate("bedroomAlt")}
-                  onClick={() => setSlide("bedrooms")}
-                />
-                {expandButton}
-              </div>
-              <div className="horizontal-img">
-                <img
-                  src="/images/outdoor/picture_3.jpg"
-                  role="button"
-                  alt={translate("outdoorAlt")}
-                  onClick={() => setSlide("outdoor")}
-                />
-                {expandButton}
-              </div>
-            </div>
-          </div>
-          <div
-            key="gallery-page-2"
-            className={cssMerge(
-              "page-container row",
-              page !== 1
-                ? page > lastPage
-                  ? "hide-slide-left"
-                  : "hide-slide-right"
-                : "visible"
-            )}
-          >
-            <div className="row col-7 horizontal-container">
-              <div className="horizontal-img">
-                <img
-                  src="/images/kitchen/picture_1.jpg"
-                  role="button"
-                  alt={translate("kitchenAlt")}
-                  onClick={() => setSlide("kitchen")}
-                />
-                {expandButton}
-              </div>
-              <div className="horizontal-img">
-                <img
-                  src="/images/environment/picture_1.jpg"
-                  role="button"
-                  alt={translate("environmentAlt")}
-                  onClick={() => setSlide("environment")}
-                />
-                {expandButton}
-              </div>
-            </div>
-            <div className="vertical-img right col-5">
+            <div className="horizontal-img">
               <img
-                src="/images/dining-room/picture_1.jpg"
+                src="/images/outdoor/picture_3.jpg"
                 role="button"
-                alt={translate("diningRoomAlt")}
-                onClick={() => setSlide("dining-room")}
+                alt={translate("outdoorAlt")}
+                onClick={() => setSlide("outdoor")}
               />
               {expandButton}
             </div>
           </div>
-          <button
-            className="next"
-            hidden={page + 1 >= nbPages}
-            onClick={() => changePage(page + 1)}
-          >
-            <i className="fas fa-chevron-right fa-2x"></i>
-          </button>
         </div>
-        {slide && (
-          <SlideShow {...props} imagesUrl={slide} setSlide={setSlide} />
-        )}
+        <div
+          key="gallery-page-2"
+          className={cssMerge(
+            "page-container row",
+            page !== 1
+              ? page > lastPage
+                ? "hide-slide-left"
+                : "hide-slide-right"
+              : "visible"
+          )}
+        >
+          <div className="row col-7 horizontal-container">
+            <div className="horizontal-img">
+              <img
+                tabIndex={-1}
+                src="/images/kitchen/picture_1.jpg"
+                role="button"
+                alt={translate("kitchenAlt")}
+                onClick={() => setSlide("kitchen")}
+              />
+              {expandButton}
+            </div>
+            <div className="horizontal-img">
+              <img
+                tabIndex={-1}
+                src="/images/environment/picture_1.jpg"
+                role="button"
+                alt={translate("environmentAlt")}
+                onClick={() => setSlide("environment")}
+              />
+              {expandButton}
+            </div>
+          </div>
+          <div className="vertical-img right col-5">
+            <img
+              tabIndex={-1}
+              src="/images/dining-room/picture_1.jpg"
+              role="button"
+              alt={translate("diningRoomAlt")}
+              onClick={() => setSlide("dining-room")}
+            />
+            {expandButton}
+          </div>
+        </div>
+        <button
+          className="next"
+          hidden={page + 1 >= nbPages}
+          onClick={() => changePage(page + 1)}
+        >
+          <i className="fas fa-chevron-right fa-2x"></i>
+        </button>
       </div>
+      {slide && <SlideShow {...props} imagesUrl={slide} setSlide={setSlide} />}
     </section>
   );
 }
