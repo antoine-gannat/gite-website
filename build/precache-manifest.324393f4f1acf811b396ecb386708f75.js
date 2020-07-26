6767e446dfd1c3a8e3a368a36ade537e6fbef63b
Antoine GANNAT <antoine.gannat@gmail.com>
2020-07-26 12:42:27 +0200

    initial

---
+++ build/precache-manifest.324393f4f1acf811b396ecb386708f75.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "970724310dfce1c6ae4cc2e11bee41a4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "da021755883be06824cb",
+    "url": "/static/css/main.ba727c10.chunk.css"
+  },
+  {
+    "revision": "2346bd3f0a4facaa1223",
+    "url": "/static/js/2.a87781b6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.a87781b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da021755883be06824cb",
+    "url": "/static/js/main.279e68a9.chunk.js"
+  },
+  {
+    "revision": "3bc2c664d3dd0434ecc1",
+    "url": "/static/js/runtime-main.5674e532.js"
+  }
+]);
