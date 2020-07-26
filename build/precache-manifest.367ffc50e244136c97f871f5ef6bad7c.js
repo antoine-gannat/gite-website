9e415da90ab64121622cad470636395d8b10d70a
Antoine Gannat <antoine.gannat@epitech.eu>
2020-07-26 19:49:31 +0200

    new build

---
+++ build/precache-manifest.367ffc50e244136c97f871f5ef6bad7c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49452bc5fd49beaccbda657127b2e2f5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fcd7282d3d54e87fa2a5",
+    "url": "/static/css/main.d67819aa.chunk.css"
+  },
+  {
+    "revision": "2346bd3f0a4facaa1223",
+    "url": "/static/js/2.a87781b6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.a87781b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fcd7282d3d54e87fa2a5",
+    "url": "/static/js/main.1842010d.chunk.js"
+  },
+  {
+    "revision": "3bc2c664d3dd0434ecc1",
+    "url": "/static/js/runtime-main.5674e532.js"
+  }
+]);
