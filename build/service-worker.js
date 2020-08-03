f0404ba9d2a507e416af0c5101687ce7f449fde8
Antoine Gannat <antoine.gannat@epitech.eu>
2020-08-03 20:03:16 +0200

    new build

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.324393f4f1acf811b396ecb386708f75.js"
+  "/precache-manifest.cafabee630117a38eba7849d825184d3.js"
 );
 
 self.addEventListener('message', (event) => {
